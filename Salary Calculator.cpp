4b77a806b5f475dc8e6ef0892ee998c3aef7a7d9
Furqan Faisal <160280549+Furqan131@users.noreply.github.com>
2024-11-13 16:58:10 +0500

    Salary Calculator.cpp

--- Salary Calculator.cpp
+++ Salary Calculator.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-double calculateNetPay(double hoursWorked, double hourlyRate, double taxRate){
-    double grossPay = hoursWorked * hourlyRate;
-    double taxDeduction = grossPay * taxRate;
-    double netPay = grossPay - taxDeduction;
+double calculateNetPay(double hoursWorked, double hourlyRate, double taxRate){ //Net Pay Function
+    double grossPay = hoursWorked * hourlyRate;  //gross pay
+    double taxDeduction = grossPay * taxRate;  //tax deduction
+    double netPay = grossPay - taxDeduction;  //net pay
     return netPay;
 }
 int main(){
     int numEmployees;
-    cout<<"Enter the number of employees: ";
+    cout<<"Enter the number of employees: ";  //taking input from user
     cin>>numEmployees;
 
     for(int i = 1; i <= numEmployees; i++){
@@ -22,7 +22,7 @@ int main(){
         cout<<"Enter tax rate(as a decimal): ";
         cin>>taxRate;
 
-        double netPay = calculateNetPay(hoursWorked, hourlyRate, taxRate);
+        double netPay = calculateNetPay(hoursWorked, hourlyRate, taxRate);  //function calling
         cout<<"Net pay for Employee "<<i<<": PKR"<<netPay<<endl;
     }
     return 0;
